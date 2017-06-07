456d067e82566fda1532d660fb1c609026fc03e9
Ashish Kapoor <ashish.kapoor@navitas.com>
2017-06-07 21:30:38 +0530

    added other files

---
+++ Daily Spell/Spellings.swift
@@ -0,0 +1,18 @@
+//
+//  Spellings.swift
+//  Daily Spell
+//
+//  Created by Ashish Kapoor on 07/06/17.
+//  Copyright © 2017 Shopndo. All rights reserved.
+//
+
+import Foundation
+
+class Spellings: NSObject {
+    var words: String?
+    
+    init (words: String) {
+        self.words = words
+    }
+    
+}
---
+++ Daily Spell/SpellingsVC.swift
@@ -0,0 +1,63 @@
+//
+//  ViewController.swift
+//  Daily Spell
+//
+//  Created by Ashish Kapoor on 07/06/17.
+//  Copyright © 2017 Shopndo. All rights reserved.
+//
+
+import UIKit
+
+class SpellingsVC: UIViewController {
+
+    @IBOutlet weak var spellingLabel: UILabel!
+    var words: [Spellings] = []
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        readLocallyStoredJsonFile()
+        title = "Daily Spell"
+        print(words.count)
+        spellingLabel.text = "Delicious"
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        self.view.backgroundColor = UIColor.random()
+    }
+    
+    func readLocallyStoredJsonFile () {
+        if let path = Bundle.main.path(forResource: "spellings", ofType: "json") {
+            do {
+                let jsonData = try NSData(contentsOfFile: path, options: NSData.ReadingOptions.mappedIfSafe)
+                do {
+                    let jsonResult: NSDictionary = try JSONSerialization.jsonObject(with: jsonData as Data, options: JSONSerialization.ReadingOptions.mutableContainers) as! NSDictionary
+                    if let spellings = jsonResult["spellings"] as? [String: Any] {
+                        for spelling in spellings {
+                            Spellings(words: spelling.value as! String)
+                        }
+                    }
+                } catch {}
+            } catch {}
+        }
+    }
+    
+}
+
+
+
+extension CGFloat {
+    static func random() -> CGFloat {
+        return CGFloat(arc4random()) / CGFloat(UInt32.max)
+    }
+}
+
+extension UIColor {
+    static func random() -> UIColor {
+        return UIColor(red:   .random(),
+                       green: .random(),
+                       blue:  .random(),
+                       alpha: 1.0)
+    }
+}
+
